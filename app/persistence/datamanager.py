eaa8377b775c7c23cbc9ac35dfbfedc58146eba1
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-28 14:30:33 +0000

    pycodestyle & doctring updated

--- app/persistence/datamanager.py
+++ app/persistence/datamanager.py
@@ -4,6 +4,7 @@ from persistence.ipersistencemanager import IPersistenceManager
 import os
 from db import db
 
+
 class DataManager(IPersistenceManager):
     """
     Defines the subclass DataManager that inherits from
@@ -11,9 +12,22 @@ class DataManager(IPersistenceManager):
     """
 
     def __init__(self, flag):
-        """Method used to initialize DataManager"""
+        """
+        Initializes DataManager instance.
+
+        Args:
+        flag (str): Flag parameter used to set file path.
+
+        Sets up DataManager instance with appropriate file path and
+        determines persistence mode based on environment variable
+        or defaults to 'file'.
+
+        Attributes:
+        persistence_mode (str): Mode of persistence for data storage,
+            either 'file' or 'database'.
+    """
         self.set_file_path(flag)
-        self.persistence_mode=os.getenv("PERSISTENCE_MODE", "file")
+        self.persistence_mode = os.getenv("PERSISTENCE_MODE", "file")
 
     def set_file_path(self, flag):
         """Sets in which json file data will be managed based on a flag"""
@@ -32,41 +46,101 @@ class DataManager(IPersistenceManager):
             raise ValueError(f"Unsupported flag value: {flag}")
 
     def save(self, entity):
+        """
+        Saves the entity based on the current persistence mode.
+
+        Args:
+            entity (dict or SQLAlchemy model): Entity to be saved.
+                If using 'file' mode, expects a dictionary.
+                If using 'database' mode, expects a SQLAlchemy model.
+
+        Raises:
+            ValueError: If persistence mode is not supported.
+        """
         if self.persistence_mode == "file":
             self.save_to_file(entity)
         elif self.persistence_mode == "database":
             self.save_to_database(entity)
         else:
-            raise ValueError(f"Unsupported persistence mode: {self.persistence_mode}")
+            raise ValueError(f"Unsupported persistence" +
+                             "mode: {self.persistence_mode}")
 
     def get(self, entity, id):
+        """
+        Retrieves the entity with the specified id based on the current
+        persistence mode.
+
+        Args:
+            entity (type): Type of entity to retrieve (used in database mode).
+            id (str): Unique identifier of the entity.
+
+        Returns:
+            dict or SQLAlchemy model: Retrieved entity.
+
+        Raises:
+            ValueError: If persistence mode is not supported.
+        """
         if self.persistence_mode == "file":
             return self.get_from_file(entity, id)
         elif self.persistence_mode == "database":
             return self.get_from_database(entity, id)
         else:
-            raise ValueError(f"Unsupported persistence mode: {self.persistence_mode}")
+            raise ValueError(f"Unsupported persistence" +
+                             "mode: {self.persistence_mode}")
 
     def delete(self, entity, id):
+        """
+        Deletes the entity with the specified id based on the current
+        persistence mode.
+
+        Args:
+            entity (type): Type of entity to delete (used in database mode).
+            id (str): Unique identifier of the entity.
+
+        Raises:
+            ValueError: If persistence mode is not supported.
+        """
         if self.persistence_mode == "file":
             self.delete_from_file(entity, id)
         elif self.persistence_mode == "database":
             self.delete_from_database(entity, id)
         else:
-            raise ValueError(f"Unsupported persistence mode: {self.persistence_mode}")
+            raise ValueError(f"Unsupported persistence" +
+                             "mode: {self.persistence_mode}")
 
     def update(self, entity, id):
+        """
+        Updates the entity with the specified id based on the current
+        persistence mode.
+
+        Args:
+            entity (dict or SQLAlchemy model): Updated entity.
+                If using 'file' mode, expects a dictionary.
+                If using 'database' mode, expects a SQLAlchemy model.
+            id (str): Unique identifier of the entity.
+
+        Raises:
+            ValueError: If persistence mode is not supported.
+        """
         if self.persistence_mode == "file":
             self.update_file(entity, id)
         elif self.persistence_mode == "database":
             self.update_database(entity, id)
         else:
-            raise ValueError(f"Unsupported persistence mode: {self.persistence_mode}")
-
+            raise ValueError(f"Unsupported persistence" +
+                             "mode: {self.persistence_mode}")
 
     def save_to_file(self, entity):
         """
-        Save data(entity) into a JSON file
+        Saves the entity to a JSON file.
+
+        Args:
+            entity (dict): Entity data to be saved into the file.
+
+        Raises:
+            FileNotFoundError: If the file specified by self.file_path does
+            not exist.
+            Exception: For any other unexpected error during file operation.
         """
         try:
             if os.path.isfile(self.file_path):
@@ -81,15 +155,26 @@ class DataManager(IPersistenceManager):
                 json.dump(data, f, indent=4)
 
         except FileNotFoundError as e:
-        # Gérer ou logger l'erreur selon votre besoin
             raise e
         except Exception as e:
-        # Gérer ou logger d'autres exceptions
             raise e
 
     def get_from_file(self, entity, id):
         """
-        Method used to get data(entity) from a JSON file
+        Retrieves the entity from a JSON file based on its id.
+
+        Args:
+            entity (type): Type of entity to retrieve (not directly used in
+            file
+            mode).
+            id (str): Unique identifier of the entity.
+
+        Returns:
+            dict: Retrieved entity data.
+
+        Raises:
+            FileNotFoundError: If the file specified by self.file_path does not
+            exist.
         """
         try:
             with open(self.file_path, 'r', encoding='UTF-8') as f:
@@ -102,7 +187,16 @@ class DataManager(IPersistenceManager):
 
     def delete_from_file(self, entity, id):
         """
-        Method used to delete data(entity) from a JSON file
+        Deletes the entity from a JSON file based on its id.
+
+        Args:
+            entity (type): Type of entity to delete (not directly used in file
+            mode).
+            id (str): Unique identifier of the entity.
+
+        Raises:
+            FileNotFoundError: If the file specified by self.file_path does not
+            exist.
         """
 
         try:
@@ -119,7 +213,15 @@ class DataManager(IPersistenceManager):
 
     def update_file(self, entity, id):
         """
-        Method used to update data(entity) from a JSON file
+        Updates the entity in a JSON file based on its id.
+
+        Args:
+            entity (dict): Updated entity data to replace existing data.
+            id (str): Unique identifier of the entity.
+
+        Raises:
+            FileNotFoundError: If the file specified by self.file_path does not
+            exist.
         """
         try:
             with open(self.file_path, 'r', encoding='UTF-8') as f:
@@ -136,19 +238,51 @@ class DataManager(IPersistenceManager):
             pass
 
     def save_to_database(self, entity):
+        """
+        Saves the entity to the database.
+
+        Args:
+            entity (SQLAlchemy model): Entity object to be saved.
+        """
         db.session.add(entity)
         db.session.commit()
 
     def get_from_database(self, entity, id):
+        """
+        Retrieves the entity from the database based on its id.
+
+        Args:
+            entity (type): Type of entity to retrieve (used in database mode).
+            id (str): Unique identifier of the entity.
+
+        Returns:
+            SQLAlchemy model: Retrieved entity object.
+        """
         return db.session.query(entity).filter_by(id=id).first()
 
     def delete_from_database(self, entity, id):
+        """
+        Deletes the entity from the database based on its id.
+
+        Args:
+            entity (type): Type of entity to delete (used in database mode).
+            id (str): Unique identifier of the entity.
+        """
+
         entity_to_delete = db.session.query(entity).filter_by(id=id).first()
         if entity_to_delete:
             db.session.delete(entity_to_delete)
             db.session.commit()
 
     def update_database(self, entity, id, data):
+        """
+        Updates the entity in the database based on its id.
+
+        Args:
+            entity (SQLAlchemy model): Updated entity object to replace
+            existing data.
+            id (str): Unique identifier of the entity.
+        """
         entity_to_update = db.session.query(entity).filter_by(id=id).first()
         if entity_to_update:
             for key, value in data.items():
