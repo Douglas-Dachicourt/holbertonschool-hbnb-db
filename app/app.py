aed9166fe28a1bbc1b97bd9c22ecfcd58931cd4b
Douglas Dachicourt <8815@holbertonstudents.com>
2024-06-28 14:07:45 +0000

    final model review + route import/ comments + pycodestyle adjusted

--- app/app.py
+++ app/app.py
@@ -3,21 +3,24 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from flask import Flask
 from dotenv import load_dotenv
 from db import db
+# from flask_jwt_extended import JWTManager
 
+# Models imports
 from models.users import User
 from models.amenity import Amenity
 from models.place import Place
 from models.country import Country
-#from flask_jwt_extended import JWTManager
+from models.review import Review
 
+# Logging module import to capture app logs
 import logging
 
-
+# Routes imports
 from api.user_api import user_api
 from api.country_api import country_api
 from api.place_api import place_api
 from api.amenities_api import amenities_api
-# from api.review_api import review_api
+from api.review_api import review_api
 from api.cities_api import cities_api
 
 
@@ -30,12 +33,10 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#app.config['JWT_SECRET_KEY'] = 'your_secret_key'
+# app.config['JWT_SECRET_KEY'] = 'your_secret_key'
 
 db.init_app(app)
-
-#jwt =JWTManager(app)
+# jwt =JWTManager(app)
 
 SWAGGER_URL = '/api/docs'
 API_URL = '/static/swagger.json'
@@ -49,22 +50,28 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     },
 )
 
+# Blueprints imports
 app.register_blueprint(swaggerui_blueprint)
 app.register_blueprint(user_api)
 app.register_blueprint(country_api)
 app.register_blueprint(place_api)
 app.register_blueprint(amenities_api)
-# app.register_blueprint(review_api)
+app.register_blueprint(review_api)
 app.register_blueprint(cities_api)
 
 
 if __name__ == "__main__":
     with app.app_context():
+
         logging.debug("Creating database tables...")
+
+        # Creation of our tables when app is running
         db.create_all()
+
         logging.debug("Tables created.")
 
-    port = os.getenv("PORT",5000)
+    # Using specified port (5000)
+    port = os.getenv("PORT", 5000)
 
     logging.debug(f"Starting app on port {port}")
 
